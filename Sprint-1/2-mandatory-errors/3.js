5cf168fa01eae92b74ac35393fb7f00f68c91889
hackertainment <259196870+hackertainment@users.noreply.github.com>
2026-07-31 04:33:00 +0800

    Fix datatype

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,4 +1,4 @@
-const cardNumber = 4533787178994213;
+const cardNumber = "4533787178994213";
 const last4Digits = cardNumber.slice(-4);
 
 // The last4Digits variable should store the last 4 digits of cardNumber
