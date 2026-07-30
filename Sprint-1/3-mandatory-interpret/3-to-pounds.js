a5fbecab7ade26c172a033c25782f47ebbe809b3
hackertainment <259196870+hackertainment@users.noreply.github.com>
2026-07-31 05:18:54 +0800

    Add comments

--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -25,3 +25,9 @@ console.log(`£${pounds}.${pence}`);
 
 // To begin, we can start with
 // 1. const penceString = "399p": initialises a string variable with the value "399p"
+// 2. lines 3-6 trim the trailing p from the string
+// 3. line 8 prepends 0 to the string until it is at least 3 characters long (so that any amount less than a pound can still get the 0 later on)
+// 4. lines 9-11 extract the first part except the last 2 characters of the string as the pounds value
+// 5. lines 14-15 extract the last 2 characters of the string as the remaining pence value
+// 6. line 16 appends 0 to the pence until it is at least 2 characters long (but it is not needed as step 3 guarenteed the length of string must be longer than 3 characters)
+// 7. line 18 prints out the formatted amount as pounds with 2 decimal places to the terminal
