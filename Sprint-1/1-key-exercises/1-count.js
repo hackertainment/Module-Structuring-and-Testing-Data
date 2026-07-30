c9e3c82bc07d3f0dee160ea8da5003c4b5c03e8a
hackertainment <259196870+hackertainment@users.noreply.github.com>
2026-07-31 03:52:51 +0800

    Describe statement

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -1,6 +1,6 @@
 let count = 0;
 
-count = count + 1;
+count = count + 1; // add the value of count by 1 and then assign the new value back to the count variable
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
