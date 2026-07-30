31000459c8b0bb20b7212954a8bbe4fd8dfe9e28
hackertainment <259196870+hackertainment@users.noreply.github.com>
2026-07-31 04:49:30 +0800

    Add comments

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -2,7 +2,7 @@ let carPrice = "10,000";
 let priceAfterOneYear = "8,543";
 
 carPrice = Number(carPrice.replaceAll(",", ""));
-priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
 
 const priceDifference = carPrice - priceAfterOneYear;
 const percentageChange = (priceDifference / carPrice) * 100;
@@ -12,11 +12,16 @@ console.log(`The percentage change is ${percentageChange}`);
 // Read the code and then answer the questions below
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
+    // total 5 function calls in lines 4, 5, and 10 respectively.
 
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
+    // Syntax Error in line 5: argument list should be separated by comma
 
 // c) Identify all the lines that are variable reassignment statements
+    // lines 4 and 5
 
 // d) Identify all the lines that are variable declarations
+    // lines 1, 2, 7, and 8
 
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+    // remove all comma separater in the string, so that it can be converted into a number afterwards
