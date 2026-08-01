3a31a277da61e0f3d15eb47c3d0e21f57211c74f
hackertainment <259196870+hackertainment@users.noreply.github.com>
2026-08-01 10:39:02 +0100

    Fix Syntax Error

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,19 @@
 // Predict and explain first...
 //  =============> write your prediction here
+// line 8: redeclaration of formal parameter str
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
-
+/*
 function capitalise(str) {
   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
-
+*/
 // =============> write your explanation here
+// str has already been declared in line 7 as input of function
 // =============> write your new code here
+function capitalise(str) {
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return str;
+}
