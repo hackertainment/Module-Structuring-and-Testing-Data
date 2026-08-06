f4cffeec788abe2b44ae10ff12b2506a10b8f866
hackertainment <259196870+hackertainment@users.noreply.github.com>
2026-08-06 12:01:30 +0100

    rewrite get card value test

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -11,9 +11,22 @@ test(`Should return 11 when given an ace card`, () => {
 
 // Suggestion: Group the remaining test data into these categories:
 //   Number Cards (2-10)
+test(`Should return its numeric value when given a number card`, () => {
+  expect(getCardValue("9♠")).toEqual(9);
+  expect(getCardValue("6♥")).toEqual(6);
+  expect(getCardValue("2♦")).toEqual(2);
+  expect(getCardValue("10♣")).toEqual(10);
+});
 //   Face Cards (J, Q, K)
+test(`Should return 10 when given a face card`, () => {
+  expect(getCardValue("J♠")).toEqual(10);
+  expect(getCardValue("Q♥")).toEqual(10);
+  expect(getCardValue("K♦")).toEqual(10);
+});
 //   Invalid Cards
-
+test(`Should return 11 when given an ace card`, () => {
+  expect(() => {getCardValue("invalid");}).toThrow();
+});
 // To learn how to test whether a function throws an error as expected in Jest,
 // please refer to the Jest documentation:
 // https://jestjs.io/docs/expect#tothrowerror
