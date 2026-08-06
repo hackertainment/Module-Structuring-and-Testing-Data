0e61ca0aab900af625a6fe8b97ffac34d6c924ae
hackertainment <259196870+hackertainment@users.noreply.github.com>
2026-08-06 16:13:06 +0100

    test repeat string

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,7 +1,16 @@
-function repeatStr() {
+function repeatStr(str, num) {
   // Your implementation of this function must *not* call String.prototype.repeat (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat).
   // The goal is to re-implement that function, not to use it.
-  return "hellohellohello";
+  let repeat = "";
+
+  if (num<0) {
+    throw new Error();
+  }
+  for (let i=1; i<=num; i++) {
+    repeat = repeat+str;
+  }
+
+  return repeat;
 }
 
 module.exports = repeatStr;
--- Sprint-3/2-practice-tdd/repeat-str.test.js
+++ Sprint-3/2-practice-tdd/repeat-str.test.js
@@ -20,13 +20,30 @@ test("should repeat the string count times", () => {
 // Given a target string `str` and a `count` equal to 1,
 // When the repeatStr function is called with these inputs,
 // Then it should return the original `str` without repetition.
+test("should repeat the string 1 times", () => {
+  expect(repeatStr("hello", 1)).toEqual("hello");
+});
 
 // Case: Handle count of 0:
 // Given a target string `str` and a `count` equal to 0,
 // When the repeatStr function is called with these inputs,
 // Then it should return an empty string.
+test("should repeat the string 0 times", () => {
+  expect(repeatStr("hello", 0)).toEqual("");
+});
 
 // Case: Handle negative count:
 // Given a target string `str` and a negative integer `count`,
 // When the repeatStr function is called with these inputs,
 // Then it should throw an error, as negative counts are not valid.
+test("should repeat the string negative times", () => {
+  expect(() => {repeatStr("hello", -1);}).toThrow();
+  expect(() => {repeatStr("", -1);}).toThrow();
+});
+
+// Case: Handle empty string:
+test("should repeat the string no times", () => {
+  expect(repeatStr("", 0)).toEqual("");
+  expect(repeatStr("", 1)).toEqual("");
+  expect(repeatStr("", 3)).toEqual("");
+});
