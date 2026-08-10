488cb3c6298b52a3b49d5cedba694f05c2496180
hackertainment <259196870+hackertainment@users.noreply.github.com>
2026-08-10 18:25:52 +0800

    Update test cases

--- Sprint-3/4-stretch/card-validator.test.js
+++ Sprint-3/4-stretch/card-validator.test.js
@@ -6,12 +6,13 @@ To be valid, a card must:
 - The sum of all the digits must be greater than 16.
 */
 const isValidCard = require("./card-validator");
-test("card has at least 5 characters", () => {
+test("card number is valid or not", () => {
     expect(isValidCard("9999777788880000")).toEqual(true);
     expect(isValidCard("6666666666661666")).toEqual(true);
     expect(isValidCard("a92332119c011112")).toEqual(false);
     expect(isValidCard("4444444444444444")).toEqual(false);
     expect(isValidCard("1111111111111110")).toEqual(false);
     expect(isValidCard("6666666666666661")).toEqual(false);
+    expect(isValidCard("12345678900987654321")).toEqual(false);
     expect(isValidCard("1234567890")).toEqual(false);
 });
